a8b71157d624edc77d56c65cf07ff8f836912c32
kondorie <111624921+kondorie@users.noreply.github.com>
2022-08-31 13:30:03 +0900

    第3回課題修正

--- work3_kondorie/js/script.js
+++ work3_kondorie/js/script.js
@@ -1,8 +1,9 @@
 let num = 20;
 sum = 0;
-for (i=0; i<10; i++){
-sum += i;
-}if(sum > num){
+for (i=0; i<10; ++i){
+sum = sum + i;
+}
+if(sum >= num){
     console.log('20以上');
 } else {
     console.log('20未満');
